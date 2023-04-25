56526a327dd538d950034031a47b9eccae9588f7
Jhon Hernandez <jhobahego16@hotmail.com>
2023-04-25 16:57:19 -0500

    Feat: se cargan las películas desde la api

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import '../style.css'
+
+const PREFIX = 'https://www.omdbapi.com/?'
+const API_KEY = '91044962'
 
 export default function Home () {
   const [search, setSearch] = useState()
+  const [movies, setMovies] = useState()
+
+  const getMovies = () => {
+    if (!search) return
+
+    const MOVIES_URL = `${PREFIX}&apikey=${API_KEY}&s=${search}`
+
+    return fetch(MOVIES_URL)
+      .then(res => {
+        return res.json()
+      })
+      .then(res => {
+        if (res.ok) throw new Error('no se ha encontrado la película')
+        return res.Search
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  useEffect(() => {
+    const result = getMovies()
+    if (result) {
+      result.then(movie => setMovies(movie))
+    }
+  }, [search])
 
   function handleSubmit (e) {
     e.preventDefault()
@@ -21,7 +50,25 @@ export default function Home () {
       </header>
 
       <main>
-        <p>Aquí van los resultados: {search}</p>
+        {
+          movies
+            ? (
+              <ul>
+                {
+                movies.map((el) =>
+                  (
+                    <li key={el.imdbID}>
+                      <h3>{el.Title}</h3>
+                      <p>{el.Year}</p>
+                      <img src={el.Poster} alt={el.Title} />
+                    </li>
+                  )
+                )
+              }
+              </ul>
+              )
+            : (<p>no se han encontrado películas</p>)
+        }
       </main>
     </div>
   )
