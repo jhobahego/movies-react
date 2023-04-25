ac3eb9327e68047d0fb16c23241a29c0012b74a7
Jhon Hernandez <jhobahego16@hotmail.com>
2023-04-25 15:42:53 -0500

    Feat: formulario de busqueda de peliculas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { useState } from 'react'
 import '../style.css'
 
 export default function Home () {
+  const [search, setSearch] = useState()
+
+  function handleSubmit (e) {
+    e.preventDefault()
+    const inputValue = e.target.searchInput.value
+    setSearch(inputValue)
+  }
+
   return (
-    <h1>Vista de inicio</h1>
+    <div>
+      <header>
+        <h1>Encuentra tu pelicula favorita</h1>
+        <form onSubmit={handleSubmit}>
+          <input name='searchInput' placeholder='Avengers, spiderman, black phanter...' autoFocus />
+          <button>Buscar</button>
+        </form>
+      </header>
+
+      <main>
+        <p>Aquí van los resultados: {search}</p>
+      </main>
+    </div>
   )
 }
